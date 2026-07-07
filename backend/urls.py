2710e7c3ebc77656691dab75f63189bf87d187fa
SultanaJahan25 <coursesellerbd25@gmail.com>
2026-07-08 03:02:26 +0600

    Initial backend commit

---
+++ backend/urls.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    # Task API
+    path("api/", include("tasks.urls")),
+
+    # Annotation API
+    path("api/annotations/", include("annotations.urls")),
+
+    # Auth
+    path("api/login/", TokenObtainPairView.as_view(), name="login"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
