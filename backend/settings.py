410ef13b4159b7e63dd0e0734e4e5e6a89e58e18
SultanaJahan25 <coursesellerbd25@gmail.com>
2026-07-09 10:28:59 +0600

    Update backend

--- backend/settings.py
+++ backend/settings.py
@@ -136,6 +136,12 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # Media Files
 # -----------------------------------------------------------------------------
 
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 cloudinary.config(
