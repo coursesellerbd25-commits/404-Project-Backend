8a0af5c713e6e4fe980855eb5302078df4311125
SultanaJahan25 <coursesellerbd25@gmail.com>
2026-07-09 15:08:14 +0600

    Update backend

--- annotations/views.py
+++ annotations/views.py
@@ -1,12 +1,30 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Image, Polygon
 from .serializers import ImageSerializer, PolygonSerializer
-
+import traceback
 
 # Upload Image API
 class ImageUploadView(generics.ListCreateAPIView):
     queryset = Image.objects.all().order_by("-id")
     serializer_class = ImageSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            print(traceback.format_exc())
+            return Response(
+                {
+                    "error": str(e),
+                    "type": type(e).__name__,
+                },
+                status=500,
+            )
 
 class PolygonCreateView(generics.CreateAPIView):
     queryset = Polygon.objects.all()
